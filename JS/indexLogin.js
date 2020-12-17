ca6e1f66bfc65952ed99b02d8fd4de6a30d6ba83
emilianommdavila <emilianommdavila@gmail.com>
2020-12-17 18:31:10 -0300

    alerta agregada

--- JS/indexLogin.js
+++ JS/indexLogin.js
@@ -1,52 +1,53 @@
 const loginButton = document.querySelector("#boton");
+window.alert("usuarios: docsito, el_teto, donald_gringu");
 
 console.log(loginButton);
 loginButton.addEventListener("click", comprobar);
 
 function comprobar() {
-  const ajax = new XMLHttpRequest();
+    const ajax = new XMLHttpRequest();
 
-  ajax.open("GET", "publicacion.json");
+    ajax.open("GET", "publicacion.json");
 
-  ajax.addEventListener("load", ajaxCallback);
+    ajax.addEventListener("load", ajaxCallback);
 
-  ajax.send();
+    ajax.send();
 
-  function ajaxCallback() {
-    console.log("hola");
-    if (ajax.status === 200) {
-      const respuesta = ajax.response;
-      const respuestaParseada = JSON.parse(respuesta);
-      const IngresoNuevo = new Usuario(
-        document.querySelector("#inputUsuario").value
-      );
-      IngresoNuevo.verificarUsuario(respuestaParseada.data);
+    function ajaxCallback() {
+        console.log("hola");
+        if (ajax.status === 200) {
+            const respuesta = ajax.response;
+            const respuestaParseada = JSON.parse(respuesta);
+            const IngresoNuevo = new Usuario(
+                document.querySelector("#inputUsuario").value
+            );
+            IngresoNuevo.verificarUsuario(respuestaParseada.data);
+        }
     }
-  }
 
-  class Usuario {
-    constructor(usuario, contrasena) {
-      this.usuario = usuario;
-      this.contraseña = contrasena;
+    class Usuario {
+        constructor(usuario, contrasena) {
+            this.usuario = usuario;
+            this.contraseña = contrasena;
+        }
+        nombre(data) {
+            return data.usuario;
+        }
+        verificarUsuario(usuario) {
+            let variable = usuario.find((persona) => persona.usuario == this.usuario);
+            if (variable) {
+                location.href = "cloneIg.html";
+                localStorage.setItem("usuarioDeIngreso", this.usuario);
+            } else {
+                console.log("no funca el usuario");
+                let element = document.querySelector("#error");
+                let fragment = document.createDocumentFragment();
+                let error = "El usuario no existe";
+                let p = document.createElement("p");
+                p.textContent = error;
+                fragment.appendChild(p);
+                element.appendChild(fragment);
+            }
+        }
     }
-    nombre(data) {
-      return data.usuario;
-    }
-    verificarUsuario(usuario) {
-      let variable = usuario.find((persona) => persona.usuario == this.usuario);
-      if (variable) {
-        location.href = "cloneIg.html";
-        localStorage.setItem("usuarioDeIngreso", this.usuario);
-      } else {
-        console.log("no funca el usuario");
-        let element = document.querySelector("#error");
-        let fragment = document.createDocumentFragment();
-        let error = "El usuario no existe";
-        let p = document.createElement("p");
-        p.textContent = error;
-        fragment.appendChild(p);
-        element.appendChild(fragment);
-      }
-    }
-  }
 }
